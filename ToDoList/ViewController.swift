1fd061fc6f33940eecb21766974076bf296fa4b8
Celine <cecelozach123@gmail.com>
2020-06-22 16:00:38 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Celine Lozach on 6/22/20.
+//  Copyright © 2020 Celine Lozach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
